e19c5a3609c3144bfb0766fa536fb04b9b09e326
KimJaeMin <kimjaemin@KimJaeMins-MacBook-Pro.local>
2024-02-21 17:50:16 +0900

    Remove: sys exit utils

--- utils.py
+++ utils.py
@@ -16,21 +16,15 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=chrome_options)
 
 def get_drive_values(url, times, *class_name):
-    logging.info('Getting nodes performance information')
-    try:
-        driver.get(url)
-        time.sleep(times)
-        if len(class_name) > 1:
-            list_value = {}
-            for name in class_name:
-                value = driver.find_elements(By.CLASS_NAME, name)
-                value_list = [i.text for i in value]
-                list_value[name] = value_list
-            return list_value
-        value = driver.find_elements(By.CLASS_NAME, class_name[0])
-        value_list = [i.text for i in value]
-        logging.info('Nodes performance information successfully geted.')
-        return value_list
-    except Exception as e:
-        logging.error(f'Error getting nodes performance information: {e}')
-        sys.exit(1)
+    driver.get(url)
+    time.sleep(times)
+    if len(class_name) > 1:
+        list_value = {}
+        for name in class_name:
+            value = driver.find_elements(By.CLASS_NAME, name)
+            value_list = [i.text for i in value]
+            list_value[name] = value_list
+        return list_value
+    value = driver.find_elements(By.CLASS_NAME, class_name[0])
+    value_list = [i.text for i in value]
+    return value_list
